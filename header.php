f7bce431b1da7910af138380a9261e88788a9eb8
Priscilla Reges <ninja.pri@gmail.com>
2022-01-11 21:13:17 -0300

    added comments

--- header.php
+++ header.php
@@ -33,11 +33,11 @@
 		</section><!--header-menu-->
 
 		<div class="container">
-			<div class="row align-items-center justify-content-lg-left">
-				<div class="col-lg sub">
-					<div class="row row-cols-lg-2 con">
+			<div class="row">
+				<div class="col-lg">
+					<div class="row row-cols-lg">
 						<section class="col-lg-auto header-items">
-							<div class="logo col-lg-6">
+							<div class="logo col col-lg-6">
 								<?php if (has_custom_logo()) :  ?>
 									<?php
 									// Get Custom Logo URL
@@ -57,18 +57,18 @@
 							</div><!--logo-->
 
 							<div class="search-form col-lg-6">
-								<?php if (is_active_sidebar('header-section-one')) : ?>
+								<?php if (is_active_sidebar('header-section-one')) : ?><!--from widgets section-->
 									<div class="header-section-one">
 										<?php dynamic_sidebar('header-section-one'); ?>
 									</div>
 								<?php endif; ?>
 							</div><!--search-form-->
 
-							<section class="cad-login col-lg-5">
+							<section class="cad-login col col-lg-6">
 								<button class="button-login">
-									<a href="<?php echo esc_url(home_url()); ?>/sacola" title="Sacola" class="cart-count">
-										<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ico-bag.svg" alt="Sacola" width="25" height="25">
-										<span class="d-none d-lg-inline-block">Sacola</span>
+									<a href="<?php echo esc_url(home_url()); ?>/cart" title="Cart" class="cart-count">
+										<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ico-bag.svg" alt="Cart" width="25" height="25">
+										<span class="d-none d-lg-inline-block">Cart</span>
 										<?php
 										if (WC()->cart->get_cart_contents_count() > 0) {
 											echo sprintf(WC()->cart->get_cart_contents_count());
@@ -76,20 +76,20 @@
 									</a>
 								</button>
 
-								<button class="button-login">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ico-user.svg" alt="Usuário" id="ico-user" class="ico-user" width="25" height="25"><span class="d-none d-lg-inline-block"></span>
+								<button class="button-login user">
+									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ico-user.svg" alt="User" id="ico-user" class="ico-user" width="25" height="25"><span class="d-none d-lg-inline-block"></span>
 									<?php
 									global $current_user;
 									wp_get_current_user();
 
 									if (!is_user_logged_in()) { ?>
 
-										<a href="<?php echo esc_url(home_url()); ?>/minha-conta" class="d-none d-lg-inline-block" title="Login">
-											<a href="<?php echo esc_url(home_url()); ?>/minha-conta" class="d-none d-lg-inline-block" title="Login">Login</a>
+										<a href="<?php echo esc_url(home_url()); ?>/account" class="d-none d-lg-inline-block" title="Login">
+											<a href="<?php echo esc_url(home_url()); ?>/account" class="d-none d-lg-inline-block" title="Login">Login</a>
 									<?php } else { ?>
-											<a href="<?php echo esc_url(home_url()); ?>/minha-conta" class="d-none d-lg-inline-block" title="Olá">
+											<a href="<?php echo esc_url(home_url()); ?>/account" class="d-none d-lg-inline-block" title="Hello">
 												<?php
-												echo 'Olá, ' . $current_user->display_name . "\n";
+												echo 'Hello, ' . $current_user->display_name . "\n";
 												?>
 											</a>
 										</a>
@@ -97,15 +97,15 @@
 								</button>
 							</section><!--cad-login-->
 						</section><!--header-items-->
-					</div><!--con-->
-				</div><!--sub-->
-			</div><!--justify-content-lg-left-->
+					</div><!--row-cols-lg-->
+				</div><!--col-lg-->
+			</div><!--row-->
 		</div><!--container-->
 
 		<div class="row">
 			<nav class="primary-navigation">
 				<?php
-				// Menu principal
+				// Main Menu created in functions.php
 				default_theme_nav('main', 'menu navbar', 'main-menu');
 				?>
 			</nav>
